e63c2f9f37442a8d98287af33e36860a65504dc6
phammien <phammien0801@gmail.com>
2019-04-15 16:59:52 +0700

    first commit

---
+++ app/src/main/java/bcvt/edu/vn/MyArrayAdapter.java
@@ -0,0 +1,47 @@
+package bcvt.edu.vn;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyArrayAdapter extends ArrayAdapter<Employee>
+{
+    Activity context=null;
+    ArrayList<Employee> myArray=null;
+    int layoutId;
+
+    public MyArrayAdapter(Activity context, int layoutId, ArrayList<Employee>arr) {
+        super(context, layoutId, arr);
+        this.context = context;
+        this.layoutId = layoutId;
+        this.myArray = arr;
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater= context.getLayoutInflater();
+        convertView = inflater.inflate(layoutId, null);
+
+        if(myArray.size()>0 && position>=0)
+        {
+            final TextView txtdisplay=(TextView)
+                    convertView.findViewById(R.id.txtitem);
+            final Employee emp=myArray.get(position);
+
+            txtdisplay.setText(emp.toString());
+
+            final ImageView imgitem=(ImageView) convertView.findViewById(R.id.imgitem);
+
+            if(emp.isGender())
+                imgitem.setImageResource(R.drawable.girlion);
+            else
+                imgitem.setImageResource(R.drawable.boyicon );
+        }
+
+        return convertView;
+    }
+}
